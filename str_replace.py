7daa5cec743febea463bcd29465fe83661bb22bd
Nilesh Vaghela <nvaghela@stratacache.com>
2019-06-27 17:05:51 -0700

    add new folder

--- str_replace.py
+++ str_replace.py
@@ -55,42 +55,44 @@ def replace_string(s, words):
 
 test_case_num = 1
 
-def test_me(s, words):
+def test_func(ss, words):
     global  test_case_num
-    print ("======test case {}=======".format(test_case_num))
-    print ("words = " , words)
-    print ("original = \"", s  , "\"")
-    print ("new      = \"", replace_string(s, words), "\"")
-    print("======test case {}=======\n\n".format(test_case_num))
+    o = open("result.txt", "a")
+    o.write ("======test case {}=======\n".format(test_case_num))
+    o.write ("words = {}\n".format(words))
+    o.write ("original = \"" +  ss  + "\"" + "\n")
+    o.write ("new      = \"" +  replace_string(ss, words) + "\"\n")
+    o.write("======test case {}=======\n\n".format(test_case_num))
     test_case_num += 1
+    o.close()
 
 
 def test_all(words):
     s1 = 'In {0} science, the {1} numbers always include {2}. In {3}, some {{4} define them to include {2}} and some do not.'
-    test_me(s1, words)
+    test_func(s1, words)
 
     # empty string
     s2 = ''
-    test_me(s2, words)
+    test_func(s2, words)
 
     # empty string
     s3 = '{}'
-    test_me(s3, words)
+    test_func(s3, words)
 
     # out of range of words
     s4 = '{-1}'
-    test_me(s4, words)
+    test_func(s4, words)
 
     # out of range of words
     s5 = '{55}'
-    test_me(s5, words)
+    test_func(s5, words)
 
     # surrounded by lot of other characters.
     s6 = '{{{{{   0   }}}}}'
-    test_me(s6, words)
+    test_func(s6, words)
 
     s7 = 'In {0} science, the {  1   } numbers always include {2}. In {3}, some {{4} define them to include {2}} and some do not.'
-    test_me(s7, words)
+    test_func(s7, words)
 
 words = ['computer', 'natural', 'zero', 'mathematics', 'texts']
 
